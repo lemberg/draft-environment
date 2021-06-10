a7a3315d2c3992ed2bd4246e80238cf81396d70b
Roman Paska <roman.paska@lemberg.co.uk>
2021-06-10 19:20:41 +0300

    #235 Use Ubuntu 20.04 (Focal Fossa) as a default guest OS

---
+++ tests/Unit/Config/Update/Step/Xenial2FocalTest.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lemberg\Tests\Unit\Draft\Environment\Config\Update\Step;
+
+use Composer\Composer;
+use Composer\Config as ComposerConfig;
+use Composer\IO\IOInterface;
+use Lemberg\Draft\Environment\Config\Config;
+use Lemberg\Draft\Environment\Config\Manager\UpdateManager;
+use Lemberg\Draft\Environment\Config\Update\Step\Xenial2Focal;
+use Lemberg\Draft\Environment\Utility\Filesystem;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests updating the guest OS box configuration.
+ *
+ * @link https://github.com/lemberg/draft-environment/issues/204
+ *
+ * @covers \Lemberg\Draft\Environment\Config\Update\Step\AbstractUpdateStep
+ * @covers \Lemberg\Draft\Environment\Config\Update\Step\Xenial2Focal
+ */
+final class Xenial2FocalTest extends TestCase {
+
+  /**
+   * @var \Composer\Composer
+   */
+  private $composer;
+
+  /**
+   * @var \Composer\IO\IOInterface
+   */
+  private $io;
+
+  /**
+   * @var string
+   */
+  private $root;
+
+  /**
+   * @var \Lemberg\Draft\Environment\Config\Manager\UpdateManagerInterface
+   */
+  private $configUpdateManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    $this->composer = new Composer();
+    $this->composer->setConfig(new ComposerConfig());
+    $this->io = $this->createMock(IOInterface::class);
+
+    // Mock source and target configuration directories.
+    $this->root = vfsStream::setup()->url();
+    $fs = new Filesystem();
+    $fs->mkdir(["$this->root/source", "$this->root/target"]);
+
+    $configObject = new Config("$this->root/source", "$this->root/target");
+    $this->configUpdateManager = new UpdateManager($this->composer, $this->io, $configObject);
+  }
+
+  /**
+   * Tests step weight getter.
+   */
+  final public function testGetWeight(): void {
+    $step = new Xenial2Focal($this->composer, $this->io, $this->configUpdateManager);
+    self::assertSame(9, $step->getWeight());
+  }
+
+  /**
+   * Tests update step execution.
+   *
+   * @param array<string,mixed> $config
+   * @param array<string,mixed> $expectedConfig
+   *
+   * @dataProvider updateDataProvider
+   */
+  final public function testUpdate(array $config, array $expectedConfig): void {
+    $step = new Xenial2Focal($this->composer, $this->io, $this->configUpdateManager);
+
+    $step->update($config);
+    self::assertSame($expectedConfig, $config);
+  }
+
+  /**
+   * Data provider for the ::testUpdate().
+   *
+   * @return array<int,array<int,string|array<string,mixed>>>
+   */
+  final public function updateDataProvider(): array {
+    return [
+      [
+        [],
+        [],
+      ],
+      [
+        [
+          'vagrant' => [
+            'box_version' => '>= 0',
+          ],
+        ],
+        [
+          'vagrant' => [
+            'box_version' => '>= 0',
+          ],
+        ],
+      ],
+      [
+        [
+          'vagrant' => [
+            'box' => 'ubuntu/xenial64',
+            'box_version' => '>= 0',
+          ],
+        ],
+        [
+          'vagrant' => [
+            'box' => 'ubuntu/focal64',
+            'box_version' => '>= 0',
+          ],
+        ],
+      ],
+      [
+        [
+          'vagrant' => [
+            'box' => 'ubuntu/bionic64',
+            'box_version' => '>= 0',
+          ],
+        ],
+        [
+          'vagrant' => [
+            'box' => 'ubuntu/bionic64',
+            'box_version' => '>= 0',
+          ],
+        ],
+      ],
+    ];
+  }
+
+}
